2aa7ad99f3c8442d2535d3480ebf188fead69293
Yusuke Kawamoto <novogrammer@gmail.com>
2025-06-25 19:01:42 +0900

    backendにproxyする

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',            // クライアントからくる /api/ 以下のリクエスト
+        destination: 'http://backend:4000/api/:path*', // Docker ネットワーク上の backend:4000/api/ に転送
+      },
+      {
+        source: '/admin/:path*',
+        destination: 'http://backend:4000/admin/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
